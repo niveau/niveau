af08db426155608f5abdf7ce20bf1c941e1ceea6
Peter Dotchev <dotchev@gmail.com>
2018-01-15 17:23:43 +0200

    Increase itest timeout (#4)
    * Increase itest timeout
    
    * Increase timeout for close app

--- itest/test.js
+++ itest/test.js
@@ -2,10 +2,12 @@
 
 const proc = require('child_process');
 
-describe('close', () => {
+describe('close', function() {
+  this.timeout(5000);
+  
   it('should allow the app to exit', () => {
     proc.execSync('node itest/app/close.js', {
-      timeout: 1800
+      timeout: 4000
     });
   });
 });
